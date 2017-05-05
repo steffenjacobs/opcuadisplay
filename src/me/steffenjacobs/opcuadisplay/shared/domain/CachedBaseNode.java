a2d07bbf05fd1ba94a18e182dd67ad1ef0e1d991
Steffen Jacobs <sjacobs@intern.oio.de>
2017-05-05 14:38:15 +0200

    added hashcode- and equals-methods to CachedBaseNode based on NodeId (IZIELE-3237)

--- src/me/steffenjacobs/opcuadisplay/shared/domain/CachedBaseNode.java
+++ src/me/steffenjacobs/opcuadisplay/shared/domain/CachedBaseNode.java
@@ -245,6 +245,7 @@ public class CachedBaseNode {
 		return !children.isEmpty();
 	}
 
+	/** @return a passive copy of the datastructure containing the children */
 	public CachedBaseNode[] getChildren() {
 		// clean up if necessary
 		// TODO: remove hack
@@ -317,6 +318,31 @@ public class CachedBaseNode {
 			return false;
 		}
 
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nodeId == null) ? 0 : nodeId.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CachedBaseNode other = (CachedBaseNode) obj;
+		if (nodeId == null) {
+			if (other.nodeId != null)
+				return false;
+		} else if (!nodeId.equals(other.nodeId))
+			return false;
 		return true;
 	}
 }
