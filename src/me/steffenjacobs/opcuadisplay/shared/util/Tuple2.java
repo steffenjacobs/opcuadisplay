eaf4c6f15df799d11ef2d6d0cbba473cbb9c5191
RANGERS2\sjacobs <steffen.jacobs@oio.de>
2017-05-11 14:55:19 +0200

    added a Tuple class for generic Bi-Tuples (IZIELE-3237)

---
+++ src/me/steffenjacobs/opcuadisplay/shared/util/Tuple2.java
@@ -0,0 +1,20 @@
+package me.steffenjacobs.opcuadisplay.shared.util;
+
+public class Tuple2<X, Y> {
+
+	private final X x;
+	private final Y y;
+
+	public Tuple2(X x, Y y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public X getX() {
+		return x;
+	}
+
+	public Y getY() {
+		return y;
+	}
+}
