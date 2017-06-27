7167035a18f44d799c32f6d9140efd567044f178
RANGERS2\sjacobs <steffen.jacobs@oio.de>
2017-06-27 14:20:23 +0200

    added hashcode/equals method to Tuple2-class (IZIELE-3237)

--- src/main/java/me/steffenjacobs/opcuadisplay/opcInterface/xml/Tuple2.java
+++ src/main/java/me/steffenjacobs/opcuadisplay/opcInterface/xml/Tuple2.java
@@ -17,4 +17,37 @@ public class Tuple2<X, Y> {
 	public Y getY() {
 		return y;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((x == null) ? 0 : x.hashCode());
+		result = prime * result + ((y == null) ? 0 : y.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Tuple2<?, ?> other = (Tuple2<?, ?>) obj;
+		if (x == null) {
+			if (other.x != null)
+				return false;
+		} else if (!x.equals(other.x))
+			return false;
+		if (y == null) {
+			if (other.y != null)
+				return false;
+		} else if (!y.equals(other.y))
+			return false;
+		return true;
+	}
+	
+	
 }
