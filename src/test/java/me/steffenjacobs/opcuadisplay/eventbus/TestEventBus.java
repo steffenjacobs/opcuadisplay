56221f0f95ae09bc123c2616ad7e297edef3ab64
RANGERS2\sjacobs <steffen.jacobs@oio.de>
2017-06-28 15:34:59 +0200

    moved the JUnit-Tests to the correct package (IZIELE-3237)

--- src/test/java/me/steffenjacobs/opcuadisplay/eventbus/TestEventBus.java
+++ src/test/java/me/steffenjacobs/opcuadisplay/eventbus/TestEventBus.java
@@ -1,4 +1,4 @@
-package me.steffenjacobs.opcuadisplay.shared.util;
+package me.steffenjacobs.opcuadisplay.eventbus;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertSame;
--- src/test/java/me/steffenjacobs/opcuadisplay/management/node/TestNodeNavigator.java
+++ src/test/java/me/steffenjacobs/opcuadisplay/management/node/TestNodeNavigator.java
@@ -1,4 +1,4 @@
-package me.steffenjacobs.opcuadisplay.shared.util.opcua;
+package me.steffenjacobs.opcuadisplay.management.node;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
